255db537350cab01435c0daf5750ca7c6cbd0914
Maxwell Muhanda <ajaybullec@gmail.com>
2023-02-01 23:21:05 +0300

    finished admin

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -1,9 +1,9 @@
 <template>
  
     <section class="featured-posts">
-  <PostPreview id="1" thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=300" title="Post one" preview-text="This is post one" />
-  <PostPreview id="2" thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=300" title="Post two" preview-text="This is post one" />
-  <PostPreview id="3" thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=300" title="Post three" preview-text="This is post one" />
+  <PostPreview id="1" :is-admin="isAdmin"  thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=300" title="Post one" preview-text="This is post one" />
+  <PostPreview id="2" :is-admin="isAdmin"  thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=300" title="Post two" preview-text="This is post one" />
+  <PostPreview id="3" :is-admin="isAdmin"  thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=300" title="Post three" preview-text="This is post one" />
 
 </section>
  
@@ -15,6 +15,12 @@ import PostPreview from './PostPreview.vue';
 export default{
   components:{
     PostPreview
+  },
+  props:{
+    isAdmin:{
+      type: Boolean,
+      default: false
+    }
   }
 }
 
