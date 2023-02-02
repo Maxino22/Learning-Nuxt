897fb075bc3371bfbb4ceeefcaff648edf1ac4e5
Maxwell Muhanda <ajaybullec@gmail.com>
2023-02-02 23:09:54 +0300

    to fix bug on posts props

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -1,9 +1,9 @@
 <template>
  
     <section class="featured-posts">
-  <PostPreview id="1" :is-admin="isAdmin"  thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=300" title="Post one" preview-text="This is post one" />
-  <PostPreview id="2" :is-admin="isAdmin"  thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=300" title="Post two" preview-text="This is post one" />
-  <PostPreview id="3" :is-admin="isAdmin"  thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=300" title="Post three" preview-text="This is post one" />
+  <PostPreview v-for="post in posts" :key="post.id"
+  :id="post.id" :is-admin="isAdmin"  :thumbnail="post.thumbnail" :title="post.title" :preview-text="post.previewText" />
+  
 
 </section>
  
@@ -20,6 +20,11 @@ export default{
     isAdmin:{
       type: Boolean,
       default: false
+    },
+    posts:{
+      type: Array,
+      required: true
+
     }
   }
 }
